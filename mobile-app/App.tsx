9f3baf71d79403dcda1eee09f45264db3d2c1ab5
Stefan Gomez <stefangomez@gmail.com>
2021-09-12 19:27:42 -0700

    add base mobile app

---
+++ mobile-app/App.tsx
@@ -0,0 +1,28 @@
+import { Box, Button, Center, Heading, NativeBaseProvider, Spacer, VStack, useColorMode } from 'native-base';
+import { StyleSheet, Text, View } from 'react-native';
+
+import ChatApp from './components/ChatApp';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <ChatApp />
+    </NativeBaseProvider>
+
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- web/config.mjs
+++
@@ -1,3 +0,0 @@
-export const config = {
-  websocketURL: 'wss://ksi45cnjjb.execute-api.us-west-2.amazonaws.com/staging',
-};
