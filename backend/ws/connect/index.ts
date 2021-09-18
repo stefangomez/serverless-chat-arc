314faffbb0b68b9c057952699f52900c47a2f46f
Stefan Gomez <stefangomez@gmail.com>
2021-09-17 19:52:53 -0700

    bug: fix connect + cleanup

--- backend/ws/connect/index.ts
+++ backend/ws/connect/index.ts
@@ -1,16 +1,13 @@
-// import { ChatDatabase } from '../../shared/database';
 import { ChatDatabase } from '@architect/shared/database';
 import { LambdaHandler } from '@architect/functions/http';
-import arc from '@architect/functions';
 
 export const handler: LambdaHandler = async (event, context) => {
   console.log('ws-connect called with', event);
   const connectionId = event.requestContext.connectionId;
   const roomId = event.queryStringParameters?.roomId || 'default';
-  const createdAt = new Date();
 
   const dbInstance = await ChatDatabase.getInstance();
-  dbInstance.joinRoom(roomId, connectionId);
+  await dbInstance.joinRoom(roomId, connectionId);
 
   return { statusCode: 200, body: '' };
 };
